824e9b05bdbf010eb164b45fe16e9e4c8b856755
Khang Huynh <khang.huynh@gmail.com>
2014-09-29 18:33:47 -0400

    added Tiger struct

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Khang Huynh on 2014-09-29.
+//  Copyright (c) 2014 Khang Huynh. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+    
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
     }
 
 
