ba3f1cd94f3791c8b5e5a299eaca22bc84ad66d7
Khang Huynh <khang.huynh@gmail.com>
2014-10-01 20:13:54 -0400

    added new Tiger funcs

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -14,8 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
     
     @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var ageLabel: UILabel!
+    
     @IBOutlet weak var breedLabel: UILabel!
+    
+    @IBOutlet weak var randomFactLabel: UILabel!
     
     var myTigers:[Tiger] = []
     
@@ -31,16 +35,22 @@ class ViewController: UIViewController {
         myTiger.age = 3
         myTiger.image = UIImage(named:"BengalTiger.jpg")
         
-        myTigers.append(myTiger)
+//        myTiger.chuff()
+        myTiger.chuffANumberOfTimes(3)
+        myTiger.chuffANumberOfTimes(5, isLoud: false)
+        myTiger.age = myTiger.ageInTigerYearsFromAge(myTiger.age)
         
-        println("My tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed), and the image is \(myTiger.image)")
+        self.myTigers.append(myTiger)
         
-        myImageView.image = myTiger.image
-        nameLabel.text = myTiger.name
-        ageLabel.text = "\(myTiger.age)"
-        breedLabel.text = myTiger.breed
+        println("ViewController: My tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed), and the image is \(myTiger.image)")
         
-        currentIndex = 0
+        self.myImageView.image = myTiger.image
+        self.nameLabel.text = myTiger.name
+        self.ageLabel.text = "\(myTiger.age)"
+        self.breedLabel.text = myTiger.breed
+        self.randomFactLabel.text = myTiger.randomFact()
+        
+        self.currentIndex = 0
         
         var secondTiger = Tiger()
         secondTiger.name = "Tigress"
@@ -48,11 +58,15 @@ class ViewController: UIViewController {
         secondTiger.age = 2
         secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
         
+        secondTiger.age = secondTiger.ageInTigerYearsFromAge(secondTiger.age)
+        
         var thirdTiger = Tiger()
         thirdTiger.name = "Jacob"
         thirdTiger.breed = "Malayan Tiger"
         thirdTiger.age = 4
         thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+        
+        thirdTiger.age = thirdTiger.ageInTigerYearsFromAge(thirdTiger.age)
         
         var fourthTiger = Tiger()
         fourthTiger.name = "Spar"
@@ -60,9 +74,14 @@ class ViewController: UIViewController {
         fourthTiger.age = 5
         fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
         
-        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        fourthTiger.age = fourthTiger.ageInTigerYearsFromAge(fourthTiger.age)
         
+        secondTiger.chuffANumberOfTimes(2)
+//        secondTiger.chuff()
+//        thirdTiger.chuff()
+//        fourthTiger.chuff()
         
+        self.myTigers += [secondTiger, thirdTiger, fourthTiger]
         
     }
 
@@ -72,25 +91,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        println("\(sender)")
+        
         var randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
         
-        while currentIndex == randomIndex {
+        while self.currentIndex == randomIndex {
             randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
         }
         
-        currentIndex = randomIndex
+        self.currentIndex = randomIndex
         
-        let tiger = myTigers[randomIndex]
+        let tiger = self.myTigers[randomIndex]
         
         UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
             self.myImageView.image = tiger.image
             self.nameLabel.text = tiger.name
             self.ageLabel.text = "\(tiger.age)"
             self.breedLabel.text = tiger.breed
+            self.randomFactLabel.text = tiger.randomFact()
             }, completion: {(finished: Bool) -> () in
         })
     }
-
 
 }
 
